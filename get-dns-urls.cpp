fb51138e0c856fcc41bcdc19b0f644867c0a3b47
Ákos Nikházy <604311+akosnikhazy@users.noreply.github.com>
2025-03-20 06:14:30 +0000

    Update get-dns-urls.cpp
    Making things nicer

--- get-dns-urls.cpp
+++ get-dns-urls.cpp
@@ -52,19 +52,20 @@ to a string
 */
 std::string replaceString(const std::string& str, const std::string& toReplace, const std::string& replaceWith) {
 // thx https://stackoverflow.com/questions/1494399/how-do-i-search-find-and-replace-in-a-standard-string
+	
 	std::string result = str;
-    
+
 	size_t pos = 0;
     
-    // Find all occurrences of "toReplace" and replace them with "replaceWith"
-    while ((pos = result.find(toReplace, pos)) != std::string::npos) {
-        
+	// Find all occurrences of "toReplace" and replace them with "replaceWith"
+	while ((pos = result.find(toReplace, pos)) != std::string::npos) {
+	        
 		result.replace(pos, toReplace.length(), replaceWith);
-        pos += replaceWith.length();
-		
-    }
+	        pos += replaceWith.length();
+			
+	}
     
-    return result;
+	return result;
 }
 
 /*
@@ -75,6 +76,7 @@ response data
 ==================
 */
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+	
 // thx https://gist.github.com/alghanmi/c5d7b761b2c9ab199157?permalink_comment_id=2909349#file-curl_example-cpp
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
@@ -91,84 +93,87 @@ response.
 void parseAPIRequest(const std::string requestURL,std::ofstream& csvFile) {
     
 	CURL *curl;
-    CURLcode res;
-    std::string readBuffer;
-    
+    	CURLcode res;
+    	
+	std::string readBuffer;
+
 	curl = curl_easy_init();
     
-    if (curl) {
+   	if (curl) {
 		
-        // Set URL and headers
-        curl_easy_setopt(curl, CURLOPT_URL, requestURL.c_str());
-
-        struct curl_slist *headers = NULL;
-        std::string authHeader = "X-Api-Key:" + apiKey;
+	        // Set URL and headers
+	        curl_easy_setopt(curl, CURLOPT_URL, requestURL.c_str());
+	
+	        struct curl_slist *headers = NULL;
+	        std::string authHeader = "X-Api-Key:" + apiKey;
+			
+	        headers = curl_slist_append(headers, authHeader.c_str());
+	        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+	
+	        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); // Disable SSL verification if SSH error happens
+	
+	        // Capture response
+	        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+	        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+	
+	        // Perform the request
+	        res = curl_easy_perform(curl);
+	
+	        if (res == CURLE_OK) {
 		
-        headers = curl_slist_append(headers, authHeader.c_str());
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-
-        
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); // Disable SSL verification if SSH error happens
-
-        // Capture response
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-
-        // Perform the request
-        res = curl_easy_perform(curl);
-
-        if (res == CURLE_OK) {
-            // Parse response
-            try {
-                json j = json::parse(readBuffer);
-
-				if(j.contains("error")) {
-					std::cout << std::endl  << std::endl << "NextDNS says: error"  << std::endl  << std::endl;
-					return;
-				}
-
-                // Extract the data array
-                if (j.contains("data")) {
-					
-                    for (const auto& item : j["data"]) {
+	            // Parse response
+	            try {
+	        	json j = json::parse(readBuffer);
+	
+			if(j.contains("error")) {
+				std::cout << std::endl  << std::endl << "NextDNS says: error"  << std::endl  << std::endl;
+				return;
+			}
+	
+	                // Extract the data array
+	                if (j.contains("data")) {
 						
-						std::string domain = item["domain"].get<std::string>() + ";" + std::to_string(item["queries"].get<int>());
+	                	for (const auto& item : j["data"]) {
+							
+					std::string domain = item["domain"].get<std::string>() + ";" + std::to_string(item["queries"].get<int>());
+							
+					// print to cli
+	                        	std::cout << "Domain: " << domain << std::endl;
+							
+					// put in csv file
+					csvFile << domain << std::endl;
+	                    }
+				
+			}
+	
+	                // NextDNS uses a "cursor" value to tell us where is the next page of data
+	                if (j.contains("meta") && j["meta"].contains("pagination")) {
+	
+				// if the cursor is null there are no more pages
+				if(j["meta"]["pagination"]["cursor"].is_null()) return;
 						
-						// print to cli
-                        std::cout << "Domain: " << domain << std::endl;
+				std::string cursor = j["meta"]["pagination"]["cursor"];
 						
-						// put in csv file
-						csvFile << domain << std::endl;
-                    }
-				}
-
-                // NextDNS uses a "cursor" value to tell us where is the next page of data
-                if (j.contains("meta") && j["meta"].contains("pagination")) {
-
-					// if the cursor is null there are no more pages
-					if(j["meta"]["pagination"]["cursor"].is_null()) return;
-					
-					
-					std::string cursor = j["meta"]["pagination"]["cursor"];
-					
-                    std::string nextURL = url + "&cursor=" + cursor;
-					
-					// print the url with cursor between pages of data
-					std::cout << std::endl  << std::endl << nextURL  << std::endl  << std::endl;
-                    
-					// recursion ʳᵉᶜᵘʳˢᶦᵒⁿ
-					parseAPIRequest(nextURL, csvFile);
-                    
-                }
-            }
-            catch (const json::exception& e) {
-                std::cerr << "Error parsing JSON: " << e.what() << std::endl;
-            }
+	                   	std::string nextURL = url + "&cursor=" + cursor;
+						
+				// print the url with cursor between pages of data
+				std::cout << std::endl  << std::endl << nextURL  << std::endl  << std::endl;
+	                    
+				// recursion ʳᵉᶜᵘʳˢᶦᵒⁿ
+				parseAPIRequest(nextURL, csvFile);
+	                    
+	                }
+			    
+	            } catch (const json::exception& e) {
+			    
+	                std::cerr << "Error parsing JSON: " << e.what() << std::endl;
+			    
+	            }
         } else {
 			
             std::cerr << "CURL request failed: " << curl_easy_strerror(res) << std::endl;
         
-		}
+	}
 
         // Cleanup
         curl_slist_free_all(headers);
@@ -179,13 +184,17 @@ int main(int argc, char* argv[]) {
  
 	// gate keepers for missing arguments
 	if( 1 == argc ) {
+		
 		std::cout << std::endl << "You need two arguments (get-dns-urls [setupid] [api key])";
 		return 0;
+		
 	}
 	
 	if( 2 == argc ) {
+		
 		std::cout << std::endl << "You need two arguments: missing api key (get-dns-urls [setupid] [api key])";
 		return 0;
+		
 	}
 	
 	// setup id is our file name prefix
@@ -198,17 +207,19 @@ int main(int argc, char* argv[]) {
 	apiKey = argv[2];
 	
 	if (csvFile.is_open()) {
-		
 	
 		std::cout << "Calling " + url;
 		
 		parseAPIRequest(url, csvFile);
+		
 		csvFile.close();
-	}else {
-		 std::cerr << "Unable to open file!" << std::endl;
+		
+	} else {
+		std::cerr << "Unable to open file!" << std::endl;
 	}
 
 	std::cout << std::endl << "Done and dusted!";
-    getchar();
-    return 0;
+	
+    	getchar();
+    	return 0;
 }
